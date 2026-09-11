bb589ace6164a11f0b829a3bc190b926e9940963
atul008-dev <jganguly399@gmail.com>
2026-09-11 19:57:29 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Works from "./components/Works"
 import Skills from "./components/Skills"
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 
