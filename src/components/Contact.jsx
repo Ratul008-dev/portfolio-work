95eda53ceebd4b17acea4070fec56de4a3aacc4e
atul008-dev <jganguly399@gmail.com>
2026-09-11 19:47:12 +0530

    Connect frontend with backend

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
 
     const onSubmit = async (data) => {
         try {
-            const response = await fetch("http://localhost:3000/api/contact", {
+            const response = await fetch("https://portfolio-backend-9sb1.onrender.com/api/contact", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
